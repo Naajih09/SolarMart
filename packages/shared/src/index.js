c27f698d1d479febdd1df227d3d62d9bc82f3b80
Naajih Siraj <Naajihibnsiraj@gmail.com>
2026-05-07 22:26:40 +0100

    Updates

--- packages/shared/src/index.js
+++ packages/shared/src/index.js
@@ -3,7 +3,6 @@ export {
   MobileStickyBar,
   Navbar,
   ScrollToTop,
-  ThemeToggle,
   WhatsAppFloat,
 } from "./components/Layout";
 export {
@@ -25,17 +24,9 @@ export {
   ProductsPage,
   ProductDetailPage,
   CartPage,
-  CalculatorPage,
   CheckoutPage,
-  CheckoutSuccessPage,
-  AuthPage,
-  ReferralLandingPage,
-  DashboardPage,
-  AffiliatePage,
 } from "./components/MarketplacePages";
 export { EmptyState } from "./components/pages/SharedPageParts";
-export { AuthProvider, useAuth } from "./context/AuthContext";
-export { ThemeProvider, useTheme } from "./context/ThemeContext";
 export { StoreProvider, useStore } from "./context/StoreContext";
 export { apiFetch, getToken, setToken } from "./lib/api";
 export { company, formatNaira, getRecommendation, whatsappMessage } from "./site";
